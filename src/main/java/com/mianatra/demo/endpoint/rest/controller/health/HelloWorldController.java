38ce1a0801d1b15df2020699690b8adfdb99c7ab
Ny <ralaivaonylalaina@gmail.com>
2026-07-12 14:30:55 +0300

    feat:Test

--- src/main/java/com/mianatra/demo/endpoint/rest/controller/health/HelloWorldController.java
+++ src/main/java/com/mianatra/demo/endpoint/rest/controller/health/HelloWorldController.java
@@ -1,12 +1,18 @@
 package com.mianatra.demo.endpoint.rest.controller.health;
 
+import com.mianatra.demo.service.HelloWorldService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class HelloWorldController {
+  private final HelloWorldService service;
+
   @GetMapping("/hello")
-  public String helloWorld() {
-    return " WORLD";
+  public String helloWorld(@RequestParam String name) {
+    return service.uploadHelloWorldMessage(name);
   }
 }
