49e4a66b395ae95f084e18d6aef72731eb567969
Ny <ralaivaonylalaina@gmail.com>
2026-07-12 19:08:05 +0300

    learn send mail poja

---
+++ src/main/java/com/mianatra/demo/endpoint/rest/controller/health/SendMailController.java
@@ -0,0 +1,27 @@
+package com.mianatra.demo.endpoint.rest.controller.health;
+
+import com.mianatra.demo.mail.Email;
+import com.mianatra.demo.mail.Mailer;
+import jakarta.mail.internet.InternetAddress;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class SendMailController {
+  private final Mailer mailer;
+
+  @GetMapping("/mail")
+  @SneakyThrows
+  public String helloWorld(@RequestParam String to) {
+    var email =
+        new Email(
+            new InternetAddress(to), List.of(), List.of(), "Hello world", "... world!", List.of());
+    mailer.accept(email);
+    return "... world!";
+  }
+}
